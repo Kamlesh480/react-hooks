4af5cbbc132f5364920a4fdd96791ad63e0fb8a0
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-04-30 14:31:03 +0530

    2 props in one hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [state, setState] = useState({ count: 4, theme: "blue" });
+  const count = state.count;
+  const theme = state.theme;
+
+  function decCount() {
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count - 1 };
+    });
+  }
+  function incCount() {
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count + 1 };
+    });
+  }
   return (
     <>
-      <button>-</button>
-      <span>0</span>
-      <button>-</button>
+      <button onClick={decCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incCount}>+</button>
     </>
   );
 }
